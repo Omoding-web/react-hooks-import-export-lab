e090b4e034a60a20347ef8895ea09f28e5a7eec0
Omoding-web <samuel.omoding@student.moringaschool.com>
2024-01-30 14:36:56 +0300

    Assignemnet done

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import NavBar from "./NavBar";
+import Home from "./Home";
+import About from "./About";
+import {username, city, image} from "/home/hp/react-hooks-import-export-lab/src/data/user.js";
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
+      <Home username={username} city={city} image={image} />
       <About />
     </div>
   );
